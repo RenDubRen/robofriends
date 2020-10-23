76c8cee67bd57c42037f05ae47e12151ce2b6ec7
unknown <romka_grand@mail.ru>
2020-10-24 00:28:31 +1000

    Updates

---
+++ precache-manifest.7296b7d6cc5f82da2400e2ca6d62883d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd9a71d034215c28c03ac265339ab79f",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "6bc907bcf4ba24690fbd",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "0e10427b82dd85411433",
+    "url": "/robofriends/static/css/main.2be7d6dd.chunk.css"
+  },
+  {
+    "revision": "6bc907bcf4ba24690fbd",
+    "url": "/robofriends/static/js/2.5f80a749.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/robofriends/static/js/2.5f80a749.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e10427b82dd85411433",
+    "url": "/robofriends/static/js/main.6195a6a6.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
